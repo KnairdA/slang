a654500cd3084cff93e4cf866a15e7977ff0cc94
Adrian Kummerlaender <knairda@t-online.de>
2017-04-15 18:56:35 +0200

    Clearly separate handling of core, conditional and definition primitives

--- source/primitives/core.d
+++ source/primitives/core.d
@@ -6,6 +6,28 @@ import base.stack;
 
 Token[string] variables;
 
+bool handle(string word) {
+	switch ( word ) {
+		case     "$"     : binary_op_variable_bind;      break;
+		case     "@"     : unary_op_variable_resolve;    break;
+		case     "+"     : binary_op_add;                break;
+		case     "*"     : binary_op_multiply;           break;
+		case     "/"     : binary_op_divide;             break;
+		case     "%"     : binary_op_modulo;             break;
+		case     "."     : unary_op_io_print;            break;
+		case     "pop"   : unary_op_stack_pop;           break;
+		case     "dup"   : unary_op_stack_dup;           break;
+		case     "swp"   : binary_op_stack_swp;          break;
+		case     "true"  : nullary_op_value_bool(true);  break;
+		case     "false" : nullary_op_value_bool(false); break;
+		case     "<"     : binary_cond_lt;               break;
+		case     "="     : binary_cond_eq;               break;
+		default          : return false;
+	}
+
+	return true;
+}
+
 void binary_op_variable_bind() {
 	string name     = stack.pop.get!string;
 	Token  value    = stack.pop;
@@ -91,6 +113,6 @@ void binary_cond_eq() {
 	stack.push(a == b);
 }
 
-void integral_value_bool(bool value) {
+void nullary_op_value_bool(bool value) {
 	stack.push(Token(value));
 }
--- source/primitives/eval.d
+++ source/primitives/eval.d
@@ -3,32 +3,10 @@ module primitives.eval;
 import std.variant;
 
 import base.stack;
-import primitives.core;
 
+import core        = primitives.core;
 import definition  = base.definition;
 import conditional = primitives.conditional;
-
-bool evaluate_primitive(string word) {
-	switch ( word ) {
-		case     "$"     : binary_op_variable_bind;    break;
-		case     "@"     : unary_op_variable_resolve;  break;
-		case     "+"     : binary_op_add;              break;
-		case     "*"     : binary_op_multiply;         break;
-		case     "/"     : binary_op_divide;           break;
-		case     "%"     : binary_op_modulo;           break;
-		case     "."     : unary_op_io_print;          break;
-		case     "pop"   : unary_op_stack_pop;         break;
-		case     "dup"   : unary_op_stack_dup;         break;
-		case     "swp"   : binary_op_stack_swp;        break;
-		case     "true"  : integral_value_bool(true);  break;
-		case     "false" : integral_value_bool(false); break;
-		case     "<"     : binary_cond_lt;             break;
-		case     "="     : binary_cond_eq;             break;
-		default          : return false;
-	}
-
-	return true;
-}
 
 bool evaluate(Token token) {
 	if ( definition.handle(token) ) {
@@ -42,7 +20,7 @@ bool evaluate(Token token) {
 	return token.visit!(
 		(int    value) => false,
 		(bool   value) => false,
-		(string word ) => evaluate_primitive(word)
+		(string word ) => core.handle(word)
 	);
 }
 
