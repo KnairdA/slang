fd8a28b1ee22993bf70b0f451b46ab0a631b0c17
Adrian Kummerlaender <knairda@t-online.de>
2017-04-19 22:20:17 +0200

    Automatically process files passed as arguments

--- source/app.d
+++ source/app.d
@@ -1,12 +1,23 @@
-import std.string : split;
-import std.stdio  : stdin, readln, writeln;
+import std.string    : split;
+import std.range     : dropOne;
+import std.algorithm : map, each;
+import std.stdio     : stdin, File, lines;
 
 static import machine;
 
-void main() {
+void process(ref File file) {
+	foreach ( string line; lines(file) ) {
+		line.split.each!(machine.process);
+	}
+}
+
+void main(string[] args) {
+	args
+		.dropOne
+		.map!(File)
+		.each!((File file) => file.process);
+
 	while ( !stdin.eof ) {
-		foreach ( token; stdin.readln.split ) {
-			machine.process(token);
-		}
+		stdin.readln.split.each!(machine.process);
 	}
 }
